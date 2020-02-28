281d9dd8a2b4dca243d7010feb940765851209db
Alfonso Heredia <alfonso@gmail.com>
2020-02-28 18:40:25 +0100

    ya nada

---
+++ blog/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Navigation from './components/Navigation'
+import NotesList from './components/NotesList'
+import CreateNote from './components/CreateNote'
+import Blog from './components/Blog'
+import Comentarios from './components/Comentario'
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      <div className="container p-4">
+        <Route path="/" exact component={Blog} />
+        <Route path="/note" exact component={NotesList} />
+        <Route path="/com" component={Comentarios} />
+        <Route path="/com/:id" component={Comentarios} />
+
+        <Route path="/edit/:id" component={CreateNote} />
+        <Route path="/create" component={CreateNote} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ blog/src/components/CreateNote.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+import 'react-datepicker/dist/react-datepicker.css'
+import axios from 'axios'
+
+export default class CreateNote extends Component {
+
+    state = {
+        title: '',
+        content: '',
+        author:'',
+        users: [],
+        editing: false,
+        _id: ''
+    }
+
+    onSubmit = async (e) => {
+        e.preventDefault();
+       
+            const newNote = {
+                title: this.state.title,
+                content: this.state.content,
+                author: this.state.author,        
+            };
+            axios.post('http://localhost:8001/api/notes', newNote);
+        
+        window.location.href = '/';
+
+    }
+
+    onInputChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+   
+    render() {
+        return (
+            <div className="col-md-6 offset-md-3">
+                <div className="card card-body">
+                    <h4>Create a Note</h4>
+                    <form onSubmit={this.onSubmit}>
+                        
+                        {/* Note Title */}
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Title"
+                                onChange={this.onInputChange}
+                                name="title"
+                                value={this.state.title}
+                                required />
+                        </div>
+                         {/* Note Author */}
+                         <div className="form-group">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Author"
+                                onChange={this.onInputChange}
+                                name="author"
+                                value={this.state.author}
+                                required />
+                        </div>
+                        {/* Note Content */}
+                        <div className="form-group">
+                            <textarea
+                                type="text"
+                                className="form-control"
+                                placeholder="Content"
+                                name="content"
+                                onChange={this.onInputChange}
+                                value={this.state.content}
+                                required>
+                            </textarea>
+                        </div>
+                       
+                        
+                        <button className="btn btn-primary">
+                            Save <i className="material-icons">
+                                assignment
+</i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
